447884c2d0a17cd0bbcd06bb946d7190da5e6d24
krekosiewicz <rekosiewicz.kacper@gmail.com>
2024-05-08 19:00:41 +0200

    update website, prepare for remote deploy, add some pages and content. To clean up

---
+++ web/config.js
@@ -0,0 +1,6 @@
+const config = {
+    instagramUrl: "https://instagram.com/kacper.rekosiewicz/",
+    linkedinUrl: "https://linkedin.com/in/kacper-r-96a355136/",
+    lichessUrl: "https://lichess.org/@/JajsonDerullo",
+    githubUrl: "https://github.com/krekosiewicz"
+};
